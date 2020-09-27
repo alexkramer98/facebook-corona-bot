8736a6d6790ed78ec5846c93779fe47329138910
alex <ad12cdef3846>
2020-09-27 17:33:23 +0200

    done

--- src/Command/RunCommand.php
+++ src/Command/RunCommand.php
@@ -7,11 +7,16 @@ use App\Service\TextFileExtractor;
 use Facebook\WebDriver\Exception\NoSuchElementException;
 use Facebook\WebDriver\Remote\RemoteWebElement;
 use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverKeys;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Panther\Client;
 
+/**
+ * Class RunCommand
+ * @package App\Command
+ */
 class RunCommand extends Command
 {
     /**
@@ -31,25 +36,46 @@ class RunCommand extends Command
      */
     private $commentSearchTerms;
     /**
+     * @var string
+     */
+    private $answerBase;
+    /**
      * @var Logger
      */
     private $logger;
 
+    /**
+     * RunCommand constructor.
+     * @param TextFileExtractor $textFileExtractor
+     * @param string|null $name
+     */
     public function __construct(TextFileExtractor $textFileExtractor, string $name = null)
     {
         parent::__construct($name);
         $this->client = Client::createChromeClient('./chromedriver', [
-            '--window-size=1400,1000',
+            '--window-size=1200,1000',
             '--disable-notifications'
         ]);
         $this->pagesToCrawl = $textFileExtractor->getData('config/app/pages-to-crawl.txt');
         $this->postSearchTerms = $textFileExtractor->getData('config/app/post-terms.txt');
-        $this->commentSearchTerms = $textFileExtractor->getData('config/app/comment-terms.txt');
+        $commentSearchTerms = $textFileExtractor->getData('config/app/comment-terms.txt');
+
+        foreach($commentSearchTerms as $term) {
+            $explode = explode(':', $term);
+            $this->commentSearchTerms[$explode[0]] = $explode[1];
+        }
+
+        $this->answerBase = str_replace(PHP_EOL, WebDriverKeys::SHIFT . WebDriverKeys::ENTER . WebDriverKeys::SHIFT, file_get_contents(
+            'config/app/answer-base.txt'
+        ));
         $this->logger = new Logger('log/log.txt', true, true);
     }
 
     protected static $defaultName = 'app:run';
 
+    /**
+     * @return array
+     */
     public function getFacebookCredentials(): array
     {
         return [
@@ -58,6 +84,9 @@ class RunCommand extends Command
         ];
     }
 
+    /**
+     * @param array $credentials
+     */
     private function login(array $credentials): void
     {
         $this->client->request('GET', 'https://facebook.com');
@@ -70,6 +99,9 @@ class RunCommand extends Command
         $this->logger->log('Logged in', 'Success');
     }
 
+    /**
+     * @param string $page
+     */
     private function searchForPage(string $page): void
     {
         $this->client->request('GET', 'https://facebook.com');
@@ -86,6 +118,11 @@ class RunCommand extends Command
         )->sendKeys($page . PHP_EOL);
     }
 
+    /**
+     * @param string $page
+     * @param array $terms
+     * @return array
+     */
     private function findPostsMatchingTerms(string $page, array $terms): array
     {
         $this->searchForPage($page);
@@ -151,6 +188,10 @@ class RunCommand extends Command
         return array_values($posts);
     }
 
+    /**
+     * @param RemoteWebElement $post
+     * @return array
+     */
     private function findCommentsMatchingTerms(RemoteWebElement $post): array
     {
         try {
@@ -185,7 +226,7 @@ class RunCommand extends Command
         $comments = $post->findElements(
             WebDriverBy::cssSelector('div[aria-label="Opmerking"]')
         );
-        $terms = implode('|', $this->commentSearchTerms);
+        $terms = implode('|', array_keys($this->commentSearchTerms));
         $comments = array_filter($comments, function($comment) use ($terms) {
             return preg_match('('.$terms.')', $comment->getText()) === 1;
         });
@@ -193,6 +234,9 @@ class RunCommand extends Command
         return $comments;
     }
 
+    /**
+     * @param RemoteWebElement $comment
+     */
     private function placeCommentIfNotExists(RemoteWebElement $comment): void
     {
         $subCommentsDiv = $comment->findElement(
@@ -225,7 +269,7 @@ class RunCommand extends Command
                 $this->logger->log('This comment has already been answered by bot. Skipping', 'Info');
                 return;
             }
-            $this->placeComment($subCommentsDiv);
+            $this->placeAnswer($subCommentsDiv, $comment->getText());
             return;
         } catch (NoSuchElementException $exception) {
             sleep(5);
@@ -241,10 +285,13 @@ class RunCommand extends Command
 
             sleep(5);
 
-            $this->placeComment($subCommentsDiv);
+            $this->placeAnswer($subCommentsDiv, $comment->getText());
         }
     }
 
+    /**
+     * @param RemoteWebElement $element
+     */
     private function scrollIntoView(RemoteWebElement  $element): void
     {
         $yPosition = $element
@@ -258,7 +305,22 @@ class RunCommand extends Command
         sleep(5);
     }
 
-    private function placeComment(RemoteWebElement $subCommentsDiv): void
+    private function getMatchingTerms(string $comment, array $terms): array
+    {
+        $matchingTerms = [];
+        foreach ($terms as $term) {
+            if (str_contains(strtolower($comment), strtolower($term))) {
+                $matchingTerms[] = $term;
+            }
+        }
+        return array_unique($matchingTerms);
+    }
+
+    /**
+     * @param RemoteWebElement $subCommentsDiv
+     * @param string $commentText
+     */
+    private function placeAnswer(RemoteWebElement $subCommentsDiv, string $commentText): void
     {
         $commentTextBox = $subCommentsDiv
             ->findElement(
@@ -266,11 +328,25 @@ class RunCommand extends Command
         );
         $this->scrollIntoView($commentTextBox);
         $commentTextBox->click();
-        $commentTextBox->sendKeys('TEST TEST TEST TEST TEST');
-        $this->logger->log('Placed comment!! Sleeping for 5 minutes to avoid detection', 'Success');
-        sleep(20);
+
+        $matchingTerms = $this->getMatchingTerms($commentText, array_keys($this->commentSearchTerms));
+        $debunks = [];
+        foreach ($matchingTerms as $term) {
+            $debunks[] = ucfirst($term) . ': ' . $this->commentSearchTerms[$term] . WebDriverKeys::SHIFT . WebDriverKeys::ENTER . WebDriverKeys::SHIFT;
+        }
+        $answer = str_replace('{matchingTerms}', implode(', ', $matchingTerms), $this->answerBase);
+        $answer = str_replace('{debunks}', implode(WebDriverKeys::SHIFT . WebDriverKeys::ENTER . WebDriverKeys::SHIFT, $debunks), $answer);
+
+        $commentTextBox->sendKeys($answer . PHP_EOL);
+        $this->logger->log('Placed answer!! Sleeping for 2 minutes to try to avoid rate limiting', 'Success');
+        sleep(120);
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         while (true) {
@@ -286,7 +362,6 @@ class RunCommand extends Command
                     }
                 }
             }
-            return 0;
         }
     }
 }
